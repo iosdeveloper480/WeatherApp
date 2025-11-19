8365d13a68d6d6d80228ca6304a63bf0f3b2136e
Waqas Ali <waqasaliw@gmail.com>
2025-11-20 00:59:26 +0400

    New changes.

---
+++ WeatherApp/Views/Components/WAImageView.swift
@@ -0,0 +1,18 @@
+//
+//  WAImageView.swift
+//  WeatherApp
+//
+//  Created by Waqas Ali on 11/19/25.
+//
+
+import SwiftUI
+
+struct WAImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WAImageView()
+}
